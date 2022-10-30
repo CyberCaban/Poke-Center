8f43a64f1c43319da49f80cf48e926a172da7f55
unknown <andrewgamer63@gmail.com>
2022-10-30 20:45:46 +0300

    pokemon-center

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Pokedex from "./pokedex";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>w84723894238942893</h1>
+      <Pokedex />
     </div>
   );
 }
---
+++ src/pokedex.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+const FIRST_POKEMONS = 10;
+
+type IPokemon = {
+  name: string;
+  photo: string;
+};
+
+function Pokedex() {
+  const [pokemons, setPokemons] = useState<IPokemon[]>();
+  let url = "https://pokeapi.co/api/v2/pokedex/27/";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let temp: IPokemon[] = [];
+        for (let i = 0; i < FIRST_POKEMONS; i++) {
+          getPokePhoto(data.pokemon_entries[i].pokemon_species.name).then(
+            (res) => {
+              temp.push({
+                name: data.pokemon_entries[i].pokemon_species.name,
+                photo: res,
+              });
+            }
+          );
+        }
+        setPokemons(temp);
+        console.log(pokemons);
+      });
+  }, []);
+
+  async function getPokePhoto(PokeName: string): Promise<string> {
+    const tempURL = `https://pokeapi.co/api/v2/pokemon/${PokeName}`;
+    let temp: string = "";
+    await fetch(tempURL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        temp = data.sprites.front_default;
+        // console.log(data.sprites.front_default);
+        // console.log(data);
+      });
+    return temp;
+  }
+
+  const names = pokemons?.map((item, index) => {
+    return <li key={index}>{item.name}</li>;
+  });
+
+  return (
+    <div>
+      <ol className="pokemon">{names}</ol>
+    </div>
+  );
+}
+
+export default Pokedex;
